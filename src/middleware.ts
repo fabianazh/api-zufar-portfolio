e37dc1a49d37767f337ce26837ea32acb9e09f79
fabianazh <fabianazhar216@gmail.com>
2024-07-30 14:04:46 +0700

    Adding middleware

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server'
+
+export function middleware(req: Request) {
+    const res = NextResponse.next()
+
+    res.headers.set('Access-Control-Allow-Origin', '*')
+    res.headers.set(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, OPTIONS'
+    )
+    res.headers.set(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization'
+    )
+
+    if (req.method === 'OPTIONS') {
+        return new NextResponse(null, { headers: res.headers })
+    }
+
+    return res
+}
